968686c8eab5912c054c23fff8e3880e50b13f63
bgeun <ayxltme@naver.com>
2020-11-05 10:05:38 +0900

    day2

--- day2/lib/main.dart
+++ day2/lib/main.dart
@@ -7,6 +7,9 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(),
+    ));
   }
 }
