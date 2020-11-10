bc848cb318a52be8712eb45622e1ecfa98672d44
bgeun <ayxltme@naver.com>
2020-11-10 19:44:28 +0900

    day7

---
+++ firebase_auth_login/lib/screens/main_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:firebase_auth_login/main.dart';
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(""),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(),
+        ],
+      ),
+    );
+  }
+}
