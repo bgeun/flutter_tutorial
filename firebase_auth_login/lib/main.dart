2778bc31995e05e9e07213afaa31c94c71eaec2a
bgeun <ayxltme@naver.com>
2020-11-13 11:09:09 +0900

    day8

--- firebase_auth_login/lib/main.dart
+++ firebase_auth_login/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_auth_login/data/join_or_login.dart';
 import 'package:firebase_auth_login/screens/login.dart';
+import 'package:firebase_auth_login/screens/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -8,9 +10,24 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: ChangeNotifierProvider<JoinOrLogin>.value(
-          value: JoinOrLogin(), child: AuthPage()),
-    );
+    return MaterialApp(home: Splash());
+  }
+}
+
+class Splash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<FirebaseUser>(
+        stream: FirebaseAuth.instance.onAuthStateChanged,
+        builder: (context, snapshot) {
+          if (snapshot.data == null) {
+            return ChangeNotifierProvider<JoinOrLogin>.value(
+              value: JoinOrLogin(),
+              child: AuthPage(),
+            );
+          } else {
+            return MainPage(email: snapshot.data.email);
+          }
+        });
   }
 }
--- firebase_auth_login/lib/screens/main_page.dart
+++ firebase_auth_login/lib/screens/main_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 class MainPage extends StatelessWidget {
   MainPage({this.email});
