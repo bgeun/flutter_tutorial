642c2400849ddb7e4da3674f2f7733a0ea2ce19f
bgeun <ayxltme@naver.com>
2020-11-14 23:57:03 +0900

    day9

---
+++ day9/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Book App',
+      theme: ThemeData(scaffoldBackgroundColor: Colors.white),
+      home: WelcomeScreen(),
+    );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/Bitmap.png"),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Column(
+          children: <Widget>[
+            RichText(
+                text: TextSpan(children: [
+              TextSpan(
+                  text: "flamin", style: Theme.of(context).textTheme.display3)
+            ]))
+          ],
+        ),
+      ),
+    );
+  }
+}
