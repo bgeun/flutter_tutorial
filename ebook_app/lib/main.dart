b14fa3934e358652c654e39b5c8ce164d8f6ea9a
bgeun <ayxltme@naver.com>
2020-11-16 10:46:28 +0900

    day10

--- ebook_app/lib/main.dart
+++ ebook_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:ebook_app/consttants.dart';
+import 'package:ebook_app/screens/home_screen.dart';
+import 'package:ebook_app/widgets/rounded_button.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -25,85 +27,48 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/Bitmap.png"),
-            fit: BoxFit.fill,
-          ),
+        body: Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/Bitmap.png"),
+          fit: BoxFit.fill,
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            RichText(
-                text: TextSpan(
-                    style: Theme.of(context).textTheme.display3,
-                    children: [
-                  TextSpan(
-                    text: "flamin",
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          RichText(
+              text: TextSpan(
+                  style: Theme.of(context).textTheme.display3,
+                  children: [
+                TextSpan(
+                  text: "flamin",
+                ),
+                TextSpan(
+                  text: "go.",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                )
+              ])),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * .6,
+            child: RoundedButton(
+              text: "start reading",
+              fontSize: 20,
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return HomeScreen();
+                    },
                   ),
-                  TextSpan(
-                    text: "go.",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  )
-                ])),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * .6,
-              child: RoundedButton(
-                text: "start reading",
-                fontSize: 20,
-              ),
+                );
+              },
             ),
-          ],
-        ),
+          )
+        ],
       ),
-    );
-  }
-}
-
-class RoundedButton extends StatelessWidget {
-  final String text;
-  final Function press;
-  final double verticalPadding;
-  final double fontSize;
-
-  const RoundedButton({
-    Key key,
-    this.text,
-    this.press,
-    this.verticalPadding = 16,
-    this.fontSize = 16,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: press,
-      child: Container(
-        alignment: Alignment.center,
-        margin: EdgeInsets.symmetric(vertical: 16),
-        padding:
-            EdgeInsets.symmetric(vertical: verticalPadding, horizontal: 30),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(30),
-          boxShadow: [
-            BoxShadow(
-              offset: Offset(0, 15),
-              blurRadius: 30,
-              color: Color(0xFF666666).withOpacity(.11),
-            )
-          ],
-        ),
-        child: Text(
-          text,
-          style: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
+    ));
   }
 }
---
+++ ebook_app/lib/screens/home_screen.dart
@@ -0,0 +1,148 @@
+import 'package:ebook_app/widgets/book_rating.dart';
+import 'package:flutter/material.dart';
+
+import '../consttants.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/main_page_bg.png"),
+            alignment: Alignment.topCenter,
+            fit: BoxFit.fitWidth,
+          ),
+        ),
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: size.height * 0.1,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: RichText(
+                  text: TextSpan(
+                style: Theme.of(context).textTheme.display1,
+                children: [
+                  TextSpan(text: "What are you \nreading"),
+                  TextSpan(
+                      text: "today?",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ))
+                ],
+              )),
+            ),
+            SizedBox(height: 30),
+            Container(
+              height: 245,
+              width: 202,
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      height: 221,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(29),
+                          boxShadow: [
+                            BoxShadow(
+                              offset: Offset(0, 10),
+                              blurRadius: 33,
+                              color: kShadowColor,
+                            )
+                          ]),
+                    ),
+                  ),
+                  Image.asset(
+                    "assets/images/book-1.png",
+                    width: 150,
+                  ),
+                  Positioned(
+                    top: 35,
+                    right: 10,
+                    child: Column(
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.favorite_border),
+                          onPressed: () {},
+                        ),
+                        BookRating(
+                          score: 4.9,
+                        )
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                      top: 160,
+                      child: Container(
+                        height: 85,
+                        width: 202,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.only(left: 24),
+                              child: RichText(
+                                text: TextSpan(
+                                  style: TextStyle(
+                                    color: kBlackColor,
+                                  ),
+                                  children: [
+                                    TextSpan(
+                                      text: "Crushing & Influence\n",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text: "Gary Venchuk",
+                                      style: TextStyle(
+                                        color: kLightBlackColor,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Container(
+                                  width: 101,
+                                  padding: EdgeInsets.symmetric(vertical: 10),
+                                  alignment: Alignment.center,
+                                  child: Text("Details"),
+                                ),
+                                Expanded(
+                                    child: Container(
+                                  alignment: Alignment.center,
+                                  padding: EdgeInsets.symmetric(vertical: 10),
+                                  decoration: BoxDecoration(
+                                    color: kBlackColor,
+                                  ),
+                                  child: Text(
+                                    "Read",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ))
+                              ],
+                            )
+                          ],
+                        ),
+                      ))
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
