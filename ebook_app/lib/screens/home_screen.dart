a60902f0ee4eb041e7bd892bf30325225ffed5c9
bgeun <ayxltme@naver.com>
2020-11-17 17:46:04 +0900

    day11

--- ebook_app/lib/screens/home_screen.dart
+++ ebook_app/lib/screens/home_screen.dart
@@ -112,6 +112,7 @@ class HomeScreen extends StatelessWidget {
                                 ),
                               ),
                             ),
+                            Spacer(),
                             Row(
                               children: <Widget>[
                                 Container(
@@ -121,17 +122,11 @@ class HomeScreen extends StatelessWidget {
                                   child: Text("Details"),
                                 ),
                                 Expanded(
-                                    child: Container(
-                                  alignment: Alignment.center,
-                                  padding: EdgeInsets.symmetric(vertical: 10),
-                                  decoration: BoxDecoration(
-                                    color: kBlackColor,
+                                  child: TwoSideRoundedButton(
+                                    text: "Read",
+                                    press: () {},
                                   ),
-                                  child: Text(
-                                    "Read",
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                ))
+                                )
                               ],
                             )
                           ],
@@ -141,6 +136,39 @@ class HomeScreen extends StatelessWidget {
               ),
             )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class TwoSideRoundedButton extends StatelessWidget {
+  final String text;
+  final double radious;
+  final Function press;
+  const TwoSideRoundedButton({
+    Key key,
+    this.text,
+    this.radious,
+    this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.symmetric(vertical: 10),
+        decoration: BoxDecoration(
+            color: kBlackColor,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(radious),
+              bottomRight: Radius.circular(radious),
+            )),
+        child: Text(
+          text,
+          style: TextStyle(color: Colors.white),
         ),
       ),
     );
