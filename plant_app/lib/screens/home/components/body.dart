199074779f386b9c1277a2d4f5b5841711fb7126
bgeun <ayxltme@naver.com>
2020-12-30 01:38:00 +0900

    2020.12.30

--- plant_app/lib/screens/home/components/body.dart
+++ plant_app/lib/screens/home/components/body.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:plant_app/constants.dart';
 import 'package:plant_app/screens/home/components/hearder_width_searchbox.dart';
 
 class Body extends StatelessWidget {
@@ -9,7 +12,57 @@ class Body extends StatelessWidget {
     // it enable scrolling on small device
     return SingleChildScrollView(
       child: Column(
-        children: <Widget>[HeaderWithSearchBox(size: size)],
+        children: <Widget>[
+          HeaderWithSearchBox(size: size),
+          Row(
+            children: [
+              TitleWithCustomUnderline(
+                text: "Recomended",
+              ),
+              FlatButton(
+                onPressed: () {},
+                child: Text("More"),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TitleWithCustomUnderline extends StatelessWidget {
+  const TitleWithCustomUnderline({
+    Key key,
+    this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 24,
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: kDefaultPadding / 4),
+            child: Text(
+              text,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: EdgeInsets.only(right: kDefaultPadding / 4),
+              height: 7,
+              color: kPrimaryColor.withOpacity(0.2),
+            ),
+          )
+        ],
       ),
     );
   }
