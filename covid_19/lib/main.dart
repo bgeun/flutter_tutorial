2de11f0fd15d9ddc4284e7ca135e6c646255b05f
bgeun <ayxltme@naver.com>
2021-01-07 23:11:31 +0900

    2020.01.07

--- covid_19/lib/main.dart
+++ covid_19/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:covid_19/constant.dart';
+import 'package:covid_19/widgets/counter.dart';
+import 'package:covid_19/widgets/my_header.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,6 +19,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: kBackgroundColor,
         fontFamily: "Poppins",
         textTheme: TextTheme(
+          // ignore: deprecated_member_use
           body1: TextStyle(color: kBodyTextColor),
         ),
       ),
@@ -30,24 +34,166 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          ClipPath(
-            clipper: MyClipper(),
-            child: Container(
-              height: 350,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
-                  colors: [
-                    Color(0xFF3383CD),
-                    Color(0xFF11249F),
+          MyHeader(
+            image: "assets/icons/Drcorona.svg",
+            textTop: "All you need",
+            textBottom: "is stay at home",
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            padding: EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 20,
+            ),
+            height: 60,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(25),
+              border: Border.all(
+                color: Color(0xFFE5E5E5),
+              ),
+            ),
+            child: Row(
+              children: [
+                SvgPicture.asset("assets/icons/maps-and-flags.svg"),
+                SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  child: DropdownButton(
+                      isExpanded: true,
+                      value: "Indonesia",
+                      underline: SizedBox(),
+                      icon: SvgPicture.asset("assets/icons/dropdown.svg"),
+                      items: [
+                        'Indonesia',
+                        'Bangladesh',
+                        'United States',
+                        'Japan'
+                      ].map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      onChanged: (value) {}),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    RichText(
+                      text: TextSpan(children: [
+                        TextSpan(
+                          text: "Case Update\n",
+                          style: kTitleTextstyle,
+                        ),
+                        TextSpan(
+                            text: "Newest update March 28",
+                            style: TextStyle(
+                              color: kTextLightColor,
+                            ))
+                      ]),
+                    ),
+                    Spacer(),
+                    Text(
+                      "See details",
+                      style: TextStyle(
+                        color: kPrimaryColor,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    )
                   ],
                 ),
-                image: DecorationImage(
-                  image: AssetImage("assets/images/virus.png"),
+                SizedBox(
+                  height: 20,
                 ),
-              ),
+                Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 4),
+                        blurRadius: 30,
+                        color: kShadowColor,
+                      )
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Counter(
+                        color: kInfectedColor,
+                        number: 1046,
+                        title: "Infected",
+                      ),
+                      Counter(
+                        color: kDeathColor,
+                        number: 87,
+                        title: "Deaths",
+                      ),
+                      Counter(
+                        color: kRecovercolor,
+                        number: 46,
+                        title: "Recovered",
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Spread of Virus",
+                      style: kTitleTextstyle,
+                    ),
+                    Text(
+                      "See details",
+                      style: TextStyle(
+                        color: kPrimaryColor,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    )
+                  ],
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    top: 20,
+                  ),
+                  padding: EdgeInsets.all(20),
+                  height: 178,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 30,
+                        color: kShadowColor,
+                      )
+                    ],
+                  ),
+                  child: Image.asset(
+                    "assets/images/map.png",
+                    fit: BoxFit.contain,
+                  ),
+                )
+              ],
             ),
           )
         ],
---
+++ covid_19/lib/widgets/my_header.dart
@@ -0,0 +1,77 @@
+import 'package:covid_19/constant.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'counter.dart';
+
+class MyHeader extends StatelessWidget {
+  final String image;
+  final String textTop;
+  final String textBottom;
+  const MyHeader({
+    Key key,
+    this.image,
+    this.textTop,
+    this.textBottom,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: MyClipper(),
+      child: Container(
+        padding: EdgeInsets.only(left: 40, top: 50, right: 20),
+        height: 350,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Color(0xFF3383CD),
+              Color(0xFF11249F),
+            ],
+          ),
+          image: DecorationImage(
+            image: AssetImage("assets/images/virus.png"),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: SvgPicture.asset("assets/icons/menu.svg"),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: Stack(
+                children: [
+                  SvgPicture.asset(
+                    image,
+                    width: 230,
+                    fit: BoxFit.fitWidth,
+                    alignment: Alignment.topCenter,
+                  ),
+                  Positioned(
+                    top: 20,
+                    left: 150,
+                    child: Text(
+                      "$textTop \n$textBottom.",
+                      style: kHeadingTextStyle.copyWith(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Container(), // I don't know why it can't work without container
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
