69d8b21a4b4f88f50fbb7fb0f5a3f42b6605013b
bgeun <ayxltme@naver.com>
2021-01-10 23:41:12 +0900

    2021.01.10

--- covid_19/lib/widgets/my_header.dart
+++ covid_19/lib/widgets/my_header.dart
@@ -1,4 +1,5 @@
 import 'package:covid_19/constant.dart';
+import 'package:covid_19/widgets/info_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -41,7 +42,15 @@ class MyHeader extends StatelessWidget {
           children: [
             Align(
               alignment: Alignment.topRight,
-              child: SvgPicture.asset("assets/icons/menu.svg"),
+              child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(
+                      builder: (context) {
+                        return InfoScreen();
+                      },
+                    ));
+                  },
+                  child: SvgPicture.asset("assets/icons/menu.svg")),
             ),
             SizedBox(
               height: 20,
