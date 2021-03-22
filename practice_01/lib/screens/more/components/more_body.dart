7e1570500ed3474508f393ad2c9f6518fd5024f9
bgeun <ayxltme@naver.com>
2021-03-22 14:20:16 +0900

    practice_01

--- practice_01/lib/screens/more/components/more_body.dart
+++ practice_01/lib/screens/more/components/more_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:practice_01/constants.dart';
 
 class MoreBody extends StatelessWidget {
   @override
@@ -8,12 +9,55 @@ class MoreBody extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Geun's closet",
+          "Geun's closet 👔",
         ),
         centerTitle: true,
       ),
-      body: Column(
-        children: <Widget>[],
+      body: Container(
+        margin: EdgeInsets.only(
+            left: kDefaultPadding,
+            top: kDefaultPadding / 2,
+            bottom: kDefaultPadding / 2.5),
+        width: size.width * 0.4,
+        child: Column(
+          children: <Widget>[
+            Image.asset(
+              "assets/images/image_1.png",
+              // width: size.width / 3,
+              // height: size.height / 4,
+            ),
+            Container(
+              padding: EdgeInsets.all(kDefaultPadding / 2),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 50,
+                    color: kPrimaryColor.withOpacity(0.23),
+                  ),
+                ],
+              ),
+              child: Row(
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                            text: "jacket",
+                            style: Theme.of(context).textTheme.button),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
